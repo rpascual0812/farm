195634f36eac8dcc3fda9a5c18100612b248dd6c
Rafael Pascual <rpascual0812@gmail.com>
2024-08-31 10:36:11 +0800

    Removed ngrx store

--- src/app/tabs/messages/messages.page.ts
+++ src/app/tabs/messages/messages.page.ts
@@ -1,28 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { increment, decrement, reset } from '../../store/counter/counter.actions';
 @Component({
     selector: 'app-messages',
     templateUrl: './messages.page.html',
     styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
-    count$: Observable<number>;
 
-    constructor(private store: Store<{ count: number }>) {
-        this.count$ = store.select('count');
+    constructor() {
+
     }
 
     ngOnInit() {
-    }
-
-    add() {
-        this.store.dispatch(increment());
-    }
-
-    minus() {
-        this.store.dispatch(decrement());
     }
 
 }
